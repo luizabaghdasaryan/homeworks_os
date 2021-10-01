c0d6eb54c8dc9a2921e1dbb3e982263451d6ea72
Luiza Baghdasaryan <91751513+luizabaghdasaryan@users.noreply.github.com>
2021-10-02 02:53:58 +0400

    Create print_AB_to_C.c

---
+++ homework 2/print_AB_to_C.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <sys/dir.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+
+
+
+
+void AB_to_C(const char* file1, const char* file2) {
+
+	char c;
+
+	char s[2];
+
+	int fd = open("f3", O_CREAT | 774);
+
+	
+
+	if (fd < 0)
+
+	{
+
+		return;
+
+	}
+
+	FILE* f1 = fopen(file1, "r");
+
+	if(!f1)
+
+	{
+
+		return;
+
+	}
+
+	while ((c = fgetc(f1)) != EOF) {
+
+		sprintf(s, "%c", c);
+
+
+
+		write(fd, s, 1);
+
+
+
+	}
+
+	lseek(fd, -1, SEEK_END);
+
+	FILE* f2 = fopen(file2, "r");
+
+	if(!f2)
+
+	{
+
+		return;
+
+	}
+
+	while ((c = fgetc(f2)) != EOF) {
+
+		sprintf(s, "%c", c);
+
+
+
+		write(fd, s, 1);
+
+	}
+
+	fclose(f1);
+
+	close(fd);
+
+	fclose(f2);
+
+}
+
+
+
+/*int main() {
+
+	AB_to_C("f1", "f2");
+
+}*/
+
+
+
+int main(int argc, char* argv[])
+
+{
+
+	AB_to_C(argv[1], argv[2]);
+
+}
+
