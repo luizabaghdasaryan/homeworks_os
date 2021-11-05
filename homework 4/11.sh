185be6e02b60b658afe6bd68411f588f8a381fc8
Luiza Baghdasaryan <91751513+luizabaghdasaryan@users.noreply.github.com>
2021-11-05 17:20:32 +0400

    Add files via upload

---
+++ homework 4/11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+filename=$1
+ext=$2
+if [[ -z $filename ]] || [[ -z $ext ]] 
+then
+	exit 1
+fi
+
+if [[ ! -f "$filename" ]]
+then 
+	echo "File \"$filename\" doesn't exist!"
+
+else
+	echo "original filename: $filename"
+	#filename_="${filename%%.*}"
+	#echo "renamed filename: $filename_.$ext"
+	filename_=`echo "$filename" | sed -e 's/\..*//' -e 's/$/'.$ext'/'`
+
+	mv $filename $filename_
+fi
+#kk=`echo "$filename" | sed -e 's/\..*//' -e 's/$/'.$ext'/'`
+#echo $kk
+#sed -e 's/\..*//' $filename
+
+
+
+
