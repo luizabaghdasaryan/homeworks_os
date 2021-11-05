e1cc5097771800a05912990942829f7a94405055
Luiza Baghdasaryan <91751513+luizabaghdasaryan@users.noreply.github.com>
2021-11-05 17:26:12 +0400

    Add files via upload

---
+++ homework 4/ls_directory.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+d_name=$1
+if [[ ! -d $d_name ]]
+then
+	echo "$d_name not a directory"
+else
+	echo -e "\\ \n ${PWD##*/}"
+	dir=$(ls $d_name)
+	for elem in $dir
+	do
+		if [[ -d $elem ]]
+		then
+			echo -e "\\ \n $elem"
+			ls $elem
+		else
+		echo $elem
+		fi
+	done
+fi
+
