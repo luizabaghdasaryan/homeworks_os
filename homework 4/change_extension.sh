b97c7857b2aca0dcc5b563512d39de5db070497d
Luiza Baghdasaryan <91751513+luizabaghdasaryan@users.noreply.github.com>
2021-11-05 17:21:56 +0400

    Update change_extension.sh

--- homework 4/change_extension.sh
+++ homework 4/change_extension.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+filename=$1
+ext=$2
+if [[ -z $filename ]] || [[ -z $ext ]] 
+then
+	exit 1
+fi
 
+if [[ ! -f "$filename" ]]
+then 
+	echo "File \"$filename\" doesn't exist!"
+
+else
+	echo "original filename: $filename"
+	#filename_="${filename%%.*}"
+	#echo "renamed filename: $filename_.$ext"
+	filename_=`echo "$filename" | sed -e 's/\..*//' -e 's/$/'.$ext'/'`
+
+	mv $filename $filename_
+fi
